caac115b09988f202153ecff21e8c9fe6577d8b9
Your Name <you@example.com>
2024-01-12 15:54:47 +0900

    set gameEnd screen

--- src/QuizGame.ts
+++ src/QuizGame.ts
@@ -392,8 +392,9 @@ export class QuizGame extends Phaser.Scene {
 
     //問題数が下回ったらエラー処理
     if (this.currentQuestionIndex >= this.questions.length) {
-      alert("問題文が足りないエラー");
-      return;
+      this.displayGameEnd();
+      // alert("問題文が足りないエラー");
+      this.MainSceneStep('gameEnd');
     }
     
     // this.clearScene(); //処理を軽くするには下をfirstStepに入れ、これを削除できるようにする
@@ -418,10 +419,20 @@ export class QuizGame extends Phaser.Scene {
       case 'explanation':
         this.displayExplanation();
         break;
+      case 'gameEnd':
+        this.displayGameEnd();
+        break;
       default:
         console.log('Invalid step: ' + step);
         break;
     }
+  }
+  displayGameEnd(){
+    alert("gameEnd");
+    this.add.text(100, 500, 'Game Over', {
+      fontSize: '24px',
+      color: '#ffffff'
+    });
   }
 
   displayQuestionNumber() {//上バー
@@ -1106,10 +1117,10 @@ export class QuizGame extends Phaser.Scene {
           this.MainSceneStep(this.currentMainStep);
         } else {
           // ゲーム終了
-          this.add.text(100, 500, 'Game Over', {
-            fontSize: '24px',
-            color: '#ffffff'
-          });
+          this.input.keyboard?.off('keydown', this.keydownListener);
+          this.currentMainStep = 'gameEnd';
+          this.MainSceneStep(this.currentMainStep);
+          
         }
       }
     };
